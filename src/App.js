8730c555659e4f10fa56c26ce85af36ca1213c6b
Roy Steenwijk <steenwijkhawk@gmail.com>
2020-11-29 13:50:15 +0100

    Added reasons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import ListOfReasons from './ListOfReasons';
 
 function App() {
+  let name = "Caryl";
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ListOfReasons title={name}/>
       </header>
     </div>
   );
---
+++ src/ListOfReasons.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+let reasonWhy = [
+    "She just is",
+    "She into coding",
+    "Looks like a snack"
+];
+
+class ListOfReasons extends React.Component
+{
+  constructor(props){
+    super(props);
+    this.state = {counter: 1}
+  }
+
+  increment = () => {
+    this.setState({counter: this.state.counter + 1});
+  }
+
+  render(){
+    let header1 = (<h1>Hello {this.props.title}, u cutie</h1>);
+    return (
+      <div>
+        <p>
+          Current Count: {this.state.counter}
+        </p>
+        <button onClick={this.increment}>Increment</button>
+      </div>
+    );
+  }
+}
+
+export default ListOfReasons;
+
+
